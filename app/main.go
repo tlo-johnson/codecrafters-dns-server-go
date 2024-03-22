f1bc14294dc6df3d13e9fb2e1945f813ffe69b5f
Tolu A <tolu@adesegha.ca>
2024-03-22 05:16:19 -0600

    write header section

--- app/main.go
+++ app/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
+
+type dnsMessage struct {
+  header []byte
+}
+
+func newDnsMessage() *dnsMessage {
+  return &dnsMessage {
+    header: newHeader(),
+  }
+}
+
+func (message *dnsMessage) byte() []byte {
+  return message.header[:]
+}
+
+func newHeader() []byte {
+  header := make([]byte, 12)
+  binary.BigEndian.PutUint16(header[0:2], 1234)
+  binary.BigEndian.PutUint16(header[2:4], 1 << 15)
+
+  return header
+}
 
 func main() {
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
@@ -31,10 +54,9 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 	
-		// Create an empty response
-		response := []byte{}
+    response := newDnsMessage()
 	
-		_, err = udpConn.WriteToUDP(response, source)
+    _, err = udpConn.WriteToUDP(response.byte(), source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
